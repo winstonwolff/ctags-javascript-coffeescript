10f69af7614edf8974493c1cf96548be6ed27e66
Winston <winston@steveandkate.com>
2015-04-27 22:13:51 -0700

    Refactor tests so source is defined in test near expected output.

--- ctags_test.py
+++ ctags_test.py
@@ -5,85 +5,109 @@
 
 import unittest
 import subprocess
-import pathlib
+import tempfile
 
 CTAGS_CONF = '_ctags'
 
+def ctags_for(lang_suffix, code_sample):
+    with tempfile.NamedTemporaryFile(mode='w', suffix=lang_suffix, delete=False) as f:
+        f.write(code_sample)
+
+    return f.name, _run_ctags(f.name)
+    
 def _run_ctags(sample_filename):
     out = subprocess.check_output(['ctags', 
-        '--options=NONE', # ignore other configuration files
+#        '--options=NONE', # ignore other configuration files
         '--options=' + CTAGS_CONF, 
-        '-f', '-', 
+        '-f-', 
         sample_filename])
     result = out.decode('utf-8')
-    print('--- ctags output for', sample_filename, '---\n', result)
     return result
 
 
 class CoffeescriptTest(unittest.TestCase):
 
-    def assertHasSymbol(self, symbol, vim_search_cmd, symbol_type):
+    def assertCtag(self, source_code, symbol, vim_search_cmd, symbol_type):
+        filepath, ctags_out = ctags_for('.coffee', source_code)
         self.assertIn(
-            '{symbol}\tsample.coffee\t{vim_search_cmd}\t{symbol_type}'.format(**locals()),
-            self.ctags_out)
-
-    def setUp(self):
-        self.ctags_out = _run_ctags('sample.coffee')
+            '{symbol}\t{filepath}\t{vim_search_cmd}\t{symbol_type}'.format(**locals()),
+            ctags_out)
 
     def test_class_method(self):
-        self.assertIn(
-            'create\tsample.coffee\t/^  create:  ->$/;"\tm',
-            self.ctags_out)
+        self.assertCtag(
+            '  create:  ->',
+            'create',
+            '/^  create:  ->/;"', 
+            'm')
 
     def test_class_constructor(self):
-        self.assertIn(
-            'CamperAppEditor\tsample.coffee\t/^class stratego.CamperAppEditor extends phaser.State$/;"\tc',
-            self.ctags_out)
+        self.assertCtag(
+            'class stratego.CamperAppEditor extends phaser.State',
+            'CamperAppEditor',
+            '/^class stratego.CamperAppEditor extends phaser.State/;"',
+            'c')
 
     def test_local_function(self):
-        self.assertHasSymbol(
+        self.assertCtag(
+            'local_function = (gfx, focusObj) ->',
             'local_function',
-            '/^local_function = (gfx, focusObj) ->$/;"',
+            '/^local_function = (gfx, focusObj) ->/;"',
             'f')
 
     def test_global_function(self):
-        self.assertHasSymbol(
+        self.assertCtag(
+            'window.global_function = (gfx, focusObj) ->',
             'global_function',
-            '/^window.global_function = (gfx, focusObj) ->$/;"',
+            '/^window.global_function = (gfx, focusObj) ->/;"',
             'f')
 
     def test_pkg_function(self):
-        self.assertHasSymbol(
+        self.assertCtag(
+            'window.pkg.pkg_function = (gfx, focusObj) ->',
             'pkg_function',
-            '/^window.pkg.pkg_function = (gfx, focusObj) ->$/;"',
+            '/^window.pkg.pkg_function = (gfx, focusObj) ->/;"',
             'f')
 
     def test_module_function(self):
-        self.assertHasSymbol(
+        self.assertCtag(
+            'module.module_function = (a, b) ->',
             'module_function',
-            '/^module.module_function = (a, b) ->$/;"',
+            '/^module.module_function = (a, b) ->/;"',
             'f')
 
     def test_exports_function(self):
-        self.assertHasSymbol(
+        self.assertCtag(
+            'exports.exports_function = (a, b) ->',
             'exports_function',
-            '/^exports.exports_function = (a, b) ->$/;"',
+            '/^exports.exports_function = (a, b) ->/;"',
             'f')
 
 class JavascriptTest(unittest.TestCase):
 
-    def setUp(self):
-        self.ctags_out = _run_ctags('sample.js')
+    def assertCtag(self, source_code, symbol, vim_search_cmd, symbol_type):
+        filepath, ctags_out = ctags_for('.js', source_code)
+        self.assertIn(
+            '{symbol}\t{filepath}\t{vim_search_cmd}\t{symbol_type}'.format(**locals()),
+            ctags_out)
 
     def test_local_function(self):
-        self.assertIn(
-            'B_local_function\tsample.js\t/^function B_local_function(a, b){}$/;"\tf',
-            self.ctags_out)
+        self.assertCtag(
+            'function B_local_function(a, b){}',
+            'B_local_function',
+            '/^function B_local_function(a, b){}/;"',
+            'f')
 
-    def test_object_class_constructor(self):
-        self.assertIn(
-            'object_method\tsample.js\t/^  object_method: function(){}$/;"\tu',
-            self.ctags_out)
+    def test_object_method(self):
+        self.assertCtag(
+            '''
+var object_class = {
+  constructor: function(){}
+  object_method: function(){}
+}
+            ''',
+            'object_method',
+            '/^  object_method: function(){}$/;"',
+            'u')
 
 if __name__=='__main__':
     unittest.main()
